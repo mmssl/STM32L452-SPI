ad983c14f898f57ff34c84bf4770a9eaf1e6ee13
Muharrem Süslü <muharremssl.001@gmail.com>
2023-03-31 23:01:40 +0300

    data received

--- main.c
+++ main.c
@@ -7,7 +7,8 @@ void SPI1_Config (void);
 void GPIO_Config (void); 
 void SPI_Transmit (volatile uint8_t *data, volatile int size);
 /*Private variables*/
-volatile uint8_t data[3] = {0x31,0x0a,0xff};
+volatile uint8_t data[3] = {0xf,0xa,0x31};
+volatile uint8_t temp;
 
 void SPI1_Config (void)
 {
@@ -19,7 +20,7 @@ void SPI1_Config (void)
   SPI1->CR1 |=  (1<<14) | (1<<15); // output enabled and bidirectional
   SPI1->CR1 &= ~(1<<10); // reset rxonly bit full-duplex mode
   SPI1->CR1 &= ~(1<<7); // MSB first
-  SPI1->CR1 &= ~(1<<9); // Hardware NSS is active
+  SPI1->CR1 |=  (1<<9) | (1<<8); // Hardware NSS is active
   SPI1->CR1 |=  (1<<2); // master mode
   
   // CR2 configuration
@@ -29,16 +30,17 @@ void SPI1_Config (void)
 
 void GPIO_Config (void) 
 {
-  RCC->AHB2ENR   |= (1<<0); // Enable the GPIOA
-  GPIOA->MODER   |= (2<<10) | (2<<12) | (2<<14) | (2<<8); // alternate function mode for PA5,6,7 and output mode for PA4
-  GPIOA->OSPEEDR |= (3<<10) | (3<<12) | (3<<14) | (3<<8); // high speed for all pins
-  GPIOA->AFR[0]  |= (5<<16) | (5<<20) | (5<<24) | (5<<28);
+  RCC->AHB2ENR   |=  (1<<0)  | (1<<1); // Enable the GPIOA
+  GPIOA->MODER   &= ~(0xffffffff);
+  GPIOA->MODER   |=  (2<<10) | (2<<12) | (2<<14) | (2<<8); // alternate function mode for PA5,6,7 and output mode for PA4
+  GPIOA->OSPEEDR |=  (3<<10) | (3<<12) | (3<<14) | (3<<8); // high speed for all pins
+  GPIOA->AFR[0]  |=  (5<<16) | (5<<20) | (5<<24) | (5<<28);
 }
 
 void SPI_Transmit (volatile uint8_t *data, volatile int size)
 {
-  int i=0;
-  while (i<size)
+  volatile int i=0;
+  while (i<=size)
   {
     while(!((SPI1->SR)&(1<<1))) {} // wait to TXE bit is set for the load data to DR
     SPI1->DR = data[i]; //load data to DR 
@@ -49,7 +51,7 @@ void SPI_Transmit (volatile uint8_t *data, volatile int size)
   while(!((SPI1->SR)&(1<<7))) {} // wait to BSY bit is set
 
   // Dummy reading to clear the overrun flag.
-  uint8_t temp = SPI1->DR;
+  temp = SPI1->DR;
   temp = SPI1->SR;
 }
 
@@ -60,8 +62,15 @@ int main (void)
   GPIO_Config();
 
   SPI1->CR1 |= (1<<6);
+  //GPIOA->BSRR |= (1<<4); 
   SPI_Transmit(data, 3);
+  //GPIOA->BSRR |= (1<<20); 
+  SPI1->CR1 &= ~(1<<6);
 
-  //SPI1->CR1 &= ~(1<<6);
+  while (1)
+  {
 
+
+  }
+return 0;
 }
